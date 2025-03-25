026546f5d866112e5fceaa348532d6cedb076aab
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-03-26 02:13:19 +0800

    feat: token test

--- app/routes/_page.tsx
+++ app/routes/_page.tsx
@@ -1,6 +1,15 @@
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
 import Footer from "~/components/elements/footer";
 import Navbar from "~/components/elements/navbar";
+import { getTokenFromRequest } from "~/lib/cookie";
+
+const restrictedRoutes = ['/'];
+
+export async function loader(args: LoaderFunctionArgs) {
+    const token = await getTokenFromRequest(args.request);
+    return args;
+}
 
 export default function PageLayout() {
     return (
