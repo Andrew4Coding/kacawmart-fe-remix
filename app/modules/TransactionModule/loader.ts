2d78eb7aa4c1f5d20c6da3a23ad9799b4e83b2e7
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-08 01:34:43 +0800

    fix: salah fetch :v

--- app/modules/TransactionModule/loader.ts
+++ app/modules/TransactionModule/loader.ts
@@ -1,12 +1,23 @@
 import { LoaderFunction } from "@remix-run/node";
 import { getTokenFromRequest } from "~/lib/cookie";
-import fetchServer from "~/lib/fetch";
 
 const transactionLoader: LoaderFunction = async (args) => {
     const token = await getTokenFromRequest(args.request);
-    const data = await fetchServer('/api/transaction/transactions', {}, token)
+    const url = new URL(args.request.url);
+    const fetchUrl = `${url.origin}/api/transaction/transactions`;
+    const data = await fetch(fetchUrl, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+            Cookie: args.request.headers.get("Cookie") || "",
+        },
+        credentials: "include",
+    })
 
-    return data;
+    const responseData = await data.json();
+    
+    return responseData;
 }
 
 export default transactionLoader;
