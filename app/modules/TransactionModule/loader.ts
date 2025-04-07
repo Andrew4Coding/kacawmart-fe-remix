d96ef37244dd59e3732b460e2a7d9d2bbd9a14b0
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-08 01:42:23 +0800

    feat: reusable fetch server

--- app/modules/TransactionModule/loader.ts
+++ app/modules/TransactionModule/loader.ts
@@ -1,23 +1,10 @@
 import { LoaderFunction } from "@remix-run/node";
-import { getTokenFromRequest } from "~/lib/cookie";
+import fetchServer from "~/lib/fetch";
 
 const transactionLoader: LoaderFunction = async (args) => {
-    const token = await getTokenFromRequest(args.request);
-    const url = new URL(args.request.url);
-    const fetchUrl = `${url.origin}/api/transaction/transactions`;
-    const data = await fetch(fetchUrl, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
-            Cookie: args.request.headers.get("Cookie") || "",
-        },
-        credentials: "include",
-    })
-
-    const responseData = await data.json();
+    const data = await fetchServer(args.request, "/api/transaction/transactions", {});
     
-    return responseData;
+    return data;
 }
 
 export default transactionLoader;
