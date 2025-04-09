3bf3ff589312f43dc3e0743143c52ffe9b2ffdad
Andrew4Coding <andrewdevitoaryo@gmail.com>
2025-04-10 00:07:20 +0800

    feat: implement enable is2fa logic

--- app/routes/_action.logout.tsx
+++ app/routes/_action.logout.tsx
@@ -4,6 +4,15 @@ import { getServerAuthClient } from "~/lib/auth-client";
 export async function loader(args: LoaderFunctionArgs) {
     const authClient = getServerAuthClient();
 
+
+    const session = await authClient.getSession({
+        fetchOptions: {
+            headers: {
+                Cookie: args.request.headers.get('Cookie') || '',
+            }
+        }
+    });
+
     authClient.signOut({
         fetchOptions: {
             headers: {
@@ -11,6 +20,26 @@ export async function loader(args: LoaderFunctionArgs) {
             }
         },
     });
+
+
+    const response = await fetch(`${process.env.AUTH_URL}/user/reset/email/verified`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+            Cookie: args.request.headers.get('Cookie') || '',
+        },
+        credentials: 'include',
+    })
+
+    try {
+        const data = await response.json();
+        console.log("Profile data:", data);
+    }
+
+    catch (error) {
+        console.error("Error fetching profile data:", error);
+    }
 
     return redirect('/login');
 }
