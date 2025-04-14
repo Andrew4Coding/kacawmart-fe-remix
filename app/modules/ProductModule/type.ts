f825488b9f03ef6f107d2786a3d1ba287b792d4f
kania.almyra <kania.almyra@ui.ac.id>
2025-04-14 09:37:51 +0700

    ya allah makasih udah bisa edit product

--- app/modules/ProductModule/type.ts
+++ app/modules/ProductModule/type.ts
@@ -20,4 +20,13 @@ export const productSchema = z.object({
   imageUrl: z.string().url("Image URL must be valid"),
 })
 
+export const UpdateProductSchema = z.object({
+  name: z.string().min(1, "Product name is required").optional(),
+  description: z.string().min(1, "Description is required").optional(),
+  categoryIds: z.array(z.string()).min(1, "At least one category is required").optional(),
+  price: z.number().int().positive("Price must be a positive integer").optional(),
+  stock: z.number().int().min(0, "Stock cannot be negative").optional(),
+  imageUrl: z.string().url("Image URL must be valid").optional(),
+})
+
 export type ProductData = z.infer<typeof productSchema>
